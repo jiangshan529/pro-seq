c49911b49c1cd36e87a086ffa1a5ceb36028b2fc
jiangshan529 <75197626+jiangshan529@users.noreply.github.com>
2024-08-16 21:22:16 -0400

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,2 +1,2 @@
-zcat 23022FL-12-01-09_S9_L001_R2_001.fastq.gz | fastx_clipper -a GATCGTCGGACTGTAGAACTCTGAAC -l 34 -d 1 -c -n -v -M 26 -o fastx.fastq
+zcat 23022FL-12-01-09_S9_L001_R2_001.fastq.gz | fastx_clipper -a GATCGTCGGACTGTAGAACTCTGAAC -l 34 -d 0 -c -n -v -M 26 -o fastx.fastq
 umi_tools extract --stdin=umi_test.fastq --bc-pattern=NNNNNNXXXXXXXXXXXXXXXXXXXXXXXXXXX --log=processed.log --stdout processed.fastq.gz --3prime
