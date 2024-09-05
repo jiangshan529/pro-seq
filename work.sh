1c8fea96a182b903a48dc6a51b1a52a7b1d3e671
jiangshan529 <75197626+jiangshan529@users.noreply.github.com>
2024-09-05 18:57:43 -0400

    Update work.sh

--- work.sh
+++ work.sh
@@ -7,7 +7,7 @@ cutadapt -u 7 -o ${prefix}_trim.fastq.gz ${prefix}.fastq.gz
 bowtie2 -p 16 --end-to-end --sensitive --no-unal -U ${prefix}_trim.fastq.gz -x /seq/epiprod02/kdong/SofiaSandbox/NanoNOMe/hg38.chrXYM 2>${prefix}_bowtie2.log | samtools view -@ 16 -Sb -q 20 -o ${prefix}_trim.bam
 samtools sort -@ 8 ${prefix}_trim.bam -o ${prefix}_trim_sort.bam
 samtools index ${prefix}_trim_sort.bam
-umi_tools dedup -I ${prefix}_trim_sort.bam --output-stats=${prefix}_trim_sort_dedup.txt -S ${prefix}_trim_sort_dedup.bam
+umi_tools dedup -I ${prefix}_trim_sort.bam --output-stats=${prefix}_trim_sort_dedup.txt -S ${prefix}_trim_sort_dedup.bam  > ${prefix}_dedup.log 2>&1
 bedtools bamtobed -i ${prefix}_trim_sort_dedup.bam > ${prefix}_trim_sort_dedup.bed
 awk '$6 == "+"' ${prefix}_trim_sort_dedup.bed | genomeCoverageBed -i stdin -3 -bg -g $hg38chrominfo > ${prefix}_trim_sort_dedup_hg38_plus.bedgraph
 awk '$6 == "-"' ${prefix}_trim_sort_dedup.bed | genomeCoverageBed -i stdin -3 -bg -g $hg38chrominfo > ${prefix}_trim_sort_dedup_hg38_m.bedgraph
@@ -15,3 +15,11 @@ awk '{$4=$4*-1; print}' ${prefix}_trim_sort_dedup_hg38_m.bedgraph > ${prefix}_tr
 bedGraphToBigWig ${prefix}_trim_sort_dedup_hg38_plus.bedgraph $hg38chrominfo ${prefix}_trim_sort_dedup_hg38_plus.bw
 bedGraphToBigWig ${prefix}_trim_sort_dedup_hg38_minus.bedgraph $hg38chrominfo ${prefix}_trim_sort_dedup_hg38_minus.bw
 
+### using genomecov 
+bedtools genomecov -ibam ${prefix}_trim_sort_dedup.bam -bg -strand + > ${prefix}_trim_sort_dedup_plus_genomecov.bedgraph
+bedtools genomecov -ibam ${prefix}_trim_sort_dedup.bam -bg -strand - > ${prefix}_trim_sort_dedup_m_genomecov.bedgraph
+awk '{$4=$4*-1; print}' ${prefix}_trim_sort_dedup_m_genomecov.bedgraph > ${prefix}_trim_sort_dedup_minus_genomecov.bedgraph
+sort -k1,1 -k2,2n ${prefix}_trim_sort_dedup_plus_genomecov.bedgraph > ${prefix}_trim_sort_dedup_plus_genomecov_SROT.bedgraph
+sort -k1,1 -k2,2n ${prefix}_trim_sort_dedup_minus_genomecov.bedgraph > ${prefix}_trim_sort_dedup_minus_genomecov_SORT.bedgraph
+bedGraphToBigWig ${prefix}_trim_sort_dedup_plus_genomecov_SROT.bedgraph $hg38chrominfo ${prefix}_trim_sort_dedup_plus_genomecov.bw
+bedGraphToBigWig ${prefix}_trim_sort_dedup_minus_genomecov_SORT.bedgraph $hg38chrominfo ${prefix}_trim_sort_dedup_minus_genomecov.bw
